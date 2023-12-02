1dc3cad814937c2e0fa29335f15c9057d09fbb77
Ritika-kumari-244 <117854625+Ritika-kumari-244@users.noreply.github.com>
2023-12-02 13:43:20 +0530

    error handling and adding models

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 // require ('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 // import mongoose from "mongoose";
-// import {DB_NAME} from "./constants.js 
+// import {DB_NAME} from "./constants.js
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
-
-
-
+  path: "./env",
+});
 
 connectDB
-
-
-
-
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running is at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!");
+  });
 
 /*
 import express from "express"
